5efc11baff0a1e6cccdd8c4fa61d4d6aaca71ea4
Dan Mutton <10258529+yearofthedan@users.noreply.github.com>
2023-11-12 00:27:58 +1100

    Add some basic code for google sign-in

--- lib/features/home/home_screen.dart
+++ lib/features/home/home_screen.dart
@@ -1,3 +1,7 @@
+import 'package:googleapis/drive/v3.dart' as drive;
+import 'package:google_sign_in/google_sign_in.dart' as sign_in;
+import 'package:logger/logger.dart';
+import 'package:storied/common/exceptions.dart';
 import 'package:storied/domain/project.dart';
 import 'package:storied/features/add_project/routes.dart';
 import 'package:storied/features/home/project_list.dart';
@@ -42,10 +46,33 @@ class HomeScreen extends StatelessWidget with WatchItMixin {
             children: [
               const AppTitle(),
               ProjectList(projectList, navigateToProject),
+              Container() // const GoogleLink()
               // ,
             ],
           )),
     );
+  }
+}
+
+class GoogleLink extends StatelessWidget {
+  const GoogleLink({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+        onPressed: _handleSignIn, child: const Text('Click!'));
+  }
+
+  Future<void> _handleSignIn() async {
+    try {
+      final googleSignIn =
+          sign_in.GoogleSignIn.standard(scopes: [drive.DriveApi.driveScope]);
+      final sign_in.GoogleSignInAccount? account = await googleSignIn.signIn();
+      Logger().i('User account $account');
+    } catch (error, stack) {
+      captureException(
+          exception: error, stack: stack, message: 'Error trying to sign in');
+    }
   }
 }
 
